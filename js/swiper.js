be9ce95f00acbcd7a795b8a497dc73262b66fba0
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2024-03-09 18:09:14 +0500

    second work

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+
+  pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
