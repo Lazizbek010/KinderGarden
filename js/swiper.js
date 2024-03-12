83a2f115467ad9d7a12f05bf9d35e594b9a4d962
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2024-03-13 00:41:20 +0500

    meida completed

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,9 @@
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
-
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
   pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
@@ -15,13 +18,29 @@ const swiper = new Swiper(".swiper", {
 
 const swiper2 = new Swiper(".swiper2", {
   direction: "horizontal",
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
     },
-
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },},
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -30,13 +49,29 @@ const swiper2 = new Swiper(".swiper2", {
 
 const swiper3 = new Swiper(".swiper3", {
   direction: "horizontal",
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
   pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
     },
-
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },},
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
